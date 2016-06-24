ed4534c5e5fa765779ac1d26eaae2280ecac5a81
Amanda Lenhart <AmandaLenhart@Amandas-MacBook-Pro.local>
2016-06-23 21:12:11 -0700

    added function to stop/start slider w/mouse cursor

--- js/script.js
+++ js/script.js
@@ -11,22 +11,33 @@ $(function() {
 	var $slideContainer = $slider.find('.slides');
 	var $slides = $slideContainer.find('.slide');
 
-	setInterval(function() {
+	var interval;
+
+	function startSlider(){
+		interval = setInterval(function() {
 		$slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function(){
 			currentSlide++;
 			if(currentSlide === $slides.length) {
 				currentSlide = 1;
 				$slideContainer.css('margin-left', 0);
-			}
-		});
-	}, pause);
+				}
+			});
+		}, pause);
+	}
+
+	function stopSlider(){
+		clearInterval(interval);
+	}
+
+
 	//setInterval
 	//animate margin-left
 	//if last slide, go to position 1
+	
 	//listen for when cursor is on carousel
 	//resume when cursor is not on carousel
+	$slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
 
-
-
+	startSlider();
 
 });
